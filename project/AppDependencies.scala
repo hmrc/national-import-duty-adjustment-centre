37461bcd685f614d788f7c0b11b1ff699b2a0b7d
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-05-19 15:37:43 +0100

    BAU - dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,17 +4,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.1.0"
+    "uk.gov.hmrc" %% "bootstrap-backend-play-28" % "5.3.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.1.0"   % Test,
-    "org.scalatest"          %% "scalatest"              % "3.2.3"   % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28" % "5.3.0"   % Test,
+    "org.scalatest"          %% "scalatest"              % "3.2.9"   % Test,
     "com.typesafe.play"      %% "play-test"              % current   % Test,
-    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.3.0" % Test,
+    "org.scalatestplus"      %% "mockito-3-4"            % "3.2.9.0" % Test,
     "com.vladsch.flexmark"    % "flexmark-all"           % "0.36.8"  % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0"   % "test, it",
-    "com.github.tomakehurst" %  "wiremock"               % "1.58"    % "it"
+    "com.github.tomakehurst" %  "wiremock-jre8"          % "2.26.3"  % "it"
   )
 
 }
